2f07c5e21000bbfb1eaa6e400212fb713a323471
peter279k <peter279k@gmail.com>
2017-09-11 03:28:34 +0800

    type convertion

--- src/Post.php
+++ src/Post.php
@@ -28,7 +28,7 @@ class Post {
             'post_type'     => 'post',
             'post_status'   => 'publish',
         ]);
-        $result = Valid::validateId($id);
+        $result = Valid::validateId((string)$id);
         $resultMsg = $id;
         if(!$result) {
             $resultMsg = 'Failed to post feed via WordPress API.';
