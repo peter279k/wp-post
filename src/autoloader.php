d63f4124deebd59f8702e4a4ffca15b6ea37ab32
peter279k <peter279k@gmail.com>
2017-09-11 18:28:34 +0800

    add else (autoloader)

--- src/autoloader.php
+++ src/autoloader.php
@@ -17,19 +17,22 @@ spl_autoload_register(function ($class) {
 
     // does the class use the namespace prefix?
     $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
+    if(strncmp($prefix, $class, $len) !== 0) {
         // no, move to the next registered autoloader
         return;
     }
+
     // get the relative class name
     $relativeClass = substr($class, $len);
 
     // replace the namespace prefix with the base directory, replace namespace
     // separators with directory separators in the relative class name, append
     // with .php
-    $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
+    $file = $baseDir.str_replace('\\', '/', $relativeClass).'.php';
     // if the file exists, require it
-    if (file_exists($file)) {
+    if(file_exists($file)) {
         require_once $file;
+    } else {
+        echo $file.' not found.';
     }
 });
