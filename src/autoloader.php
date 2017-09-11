47f1f84c6e3f46bbe825ea8f18ce05e94022131f
peter279k <peter279k@gmail.com>
2017-09-11 15:31:35 +0800

    change the file path

--- src/autoloader.php
+++ src/autoloader.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once __DIR__.'/../wp-load.php';
-require_once __DIR__.'/../wp-admin/includes/post.php';
-require_once __DIR__.'/../wp-admin/includes/image.php';
+require_once __DIR__.'/../../wp-load.php';
+require_once __DIR__.'/../../wp-admin/includes/post.php';
+require_once __DIR__.'/../../wp-admin/includes/image.php';
 
 // include the WP_Http class for HTTP request
-require_once __DIR__.'/../wp-includes/class-http.php';
+require_once __DIR__.'/../../wp-includes/class-http.php';
 
 spl_autoload_register(function ($class) {
     require_once __DIR__.'/'.str_replace('\\', '/', $class).'.php';
